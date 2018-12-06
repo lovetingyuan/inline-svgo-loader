028399378096e21fc65062453368ec6bc82a06af
Baitu Huang <baitu.huang@shopeemobile.com>
2018-12-06 16:35:47 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # inline-svgo-loader
 use svgo to optimize svg file, transformed as string or data-url
+
+### usage
+```bash
+npm install git+https://github.com/lovetingyuan/inline-svgo-loader.git --save-dev
+```
+
+```javascript
+{
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        loader: 'inline-svgo-loader'
+      }
+    ]
+  }
+}
+```
+
+append `dataUrl` query param to svg path to get data-url result like `data:image/svg+xml...`(not base64) when used in css `url()`
+
+eg: `background-image: url('@/assets/some.svg?dataUrl')`
---
+++ index.js
@@ -0,0 +1,23 @@
+const SVGO = require('svgo');
+const loaderUtils = require('loader-utils');
+const instance = new Map();
+
+module.exports = function svgoLoader(source) {
+  const callback = this.async();
+  const config = loaderUtils.getOptions(this);
+  if (!instance.has(config)) {
+    instance.set(config, new SVGO(config || {}));
+  }
+  instance.get(config).optimize(source, {path: this.resourcePath}).then(result => {
+    let dataUrl;
+    if (this.resourceQuery[0] === '?') {
+      dataUrl = 'dataUrl' in loaderUtils.parseQuery(this.resourceQuery);
+    }
+    const svgStr = (dataUrl ? 'data:image/svg+xml;utf8,' : '') + result.data;
+    if (this.loaderIndex) {
+      callback(null, svgStr);
+    } else {
+      callback(null, `module.exports = ${JSON.stringify(svgStr)}`);
+    }
+  });
+};
