db339cc34a61301c3a18027beafad05a4b9c50d9
Baitu Huang <baitu.huang@shopeemobile.com>
2018-12-06 17:12:04 +0800

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const SVGO = require('svgo');
 const loaderUtils = require('loader-utils');
 const instance = new Map();
+const toDataUri = require('mini-svg-data-uri');
 
 module.exports = function svgoLoader(source) {
   const callback = this.async();
@@ -9,15 +10,14 @@ module.exports = function svgoLoader(source) {
     instance.set(config, new SVGO(config || {}));
   }
   instance.get(config).optimize(source, {path: this.resourcePath}).then(result => {
-    let dataUrl;
-    if (this.resourceQuery[0] === '?') {
-      dataUrl = 'dataUrl' in loaderUtils.parseQuery(this.resourceQuery);
+    let svgStr = result.data;
+    if (this.resourceQuery[0] === '?' && ('dataUrl' in loaderUtils.parseQuery(this.resourceQuery))) {
+      svgStr = toDataUri(result.data);
     }
-    const svgStr = (dataUrl ? 'data:image/svg+xml;utf8,' : '') + result.data;
     if (this.loaderIndex) {
       callback(null, svgStr);
     } else {
       callback(null, `module.exports = ${JSON.stringify(svgStr)}`);
     }
-  });
+  }).catch(e => callback(e instanceof Error ? e : new Error(e)));
 };
