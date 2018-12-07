f607045bb979456bff3c697346636f4e88dc188b
Baitu Huang <baitu.huang@shopeemobile.com>
2018-12-07 11:10:59 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -18,11 +18,13 @@ test('svgo inline loader', function (t) {
   compiler.run((err, stats) => {
     const fs = compiler.outputFileSystem;
     const result = fs.readFileSync(path.join(compiler.outputPath, 'main.js'), 'utf8');
-    t.equal(/exports="data:image\/svg\+xml,%3csvg/.test(result), true);
+    t.equal(/exports="data:image\/svg\+xml,%3csvg /.test(result), true);
     t.equal(/exports='<svg /.test(result), true);
     t.equal(/><\/svg>/.test(result), true);
     t.equal(/xmlns="http:\/\/www.w3.org\/2000\/svg"/.test(result), true);
     t.equal(/xmlns='http:\/\/www.w3.org\/2000\/svg'/.test(result), true);
+    t.equal(/Created with Sketch/.test(result), false);
+    t.equal(/color='red' font-size='14'/.test(result), true);
     if (err || stats.hasErrors()) {
       t.fail(err || 'webpack has errors.');
     } else {
